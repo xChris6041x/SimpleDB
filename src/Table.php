0717fcee286d092f745378812910b56f53043fc1
Christopher Bishop <christopherb6041@gmail.com>
2017-02-06 14:35:57 -0600

    Formating

--- src/Table.php
+++ src/Table.php
@@ -4,11 +4,11 @@
     require_once('Database.php');
 
     /**
-	 * Used to access databases and to send queries to specific tables.
+     * Used to access databases and to send queries to specific tables.
      *
-	 * @author Christopher T. Bishop
-	 * @version 0.1.0
-	 */
+     * @author Christopher T. Bishop
+     * @version 0.1.0
+     */
     class Table {
 
         protected $db;
@@ -17,124 +17,124 @@
 
 
         public function __construct(Database $db, $name, $primaryKey) {
-			$this->db = $db;
-			$this->name = $name;
-			$this->primaryKey = $primaryKey;
-		}
+            $this->db = $db;
+            $this->name = $name;
+            $this->primaryKey = $primaryKey;
+        }
 
 
         /**
-		 * Select rows from the table.
+         * Select rows from the table.
          *
-	     * @author Christopher T. Bishop
-	     * @since 0.1.0
-		 * @param array $args
-		 * @return QueryResult
-		 */
-		public function select($args = []) {
-			return $this->db->select($this->name, $args);
-		}
+         * @author Christopher T. Bishop
+         * @since 0.1.0
+         * @param array $args
+         * @return QueryResult
+         */
+        public function select($args = []) {
+            return $this->db->select($this->name, $args);
+        }
 
         /**
-		 * Select one row which meets the condition primaryKey=$value.
+         * Select one row which meets the condition primaryKey=$value.
          *
-	     * @author Christopher T. Bishop
-	     * @since 0.1.0
-		 * @param mixed $value
-		 * @return QueryResult
-		 */
-		public function selectOne($value) {
-			return $this->select([
-					'where' => "$this->primaryKey=$value",
-					'limit' => 1
-			]);
-		}
+         * @author Christopher T. Bishop
+         * @since 0.1.0
+         * @param mixed $value
+         * @return QueryResult
+         */
+        public function selectOne($value) {
+            return $this->select([
+                'where' => "$this->primaryKey=$value",
+                'limit' => 1
+            ]);
+        }
 
         /**
-		 * Count the number of rows that meet the condition.
+         * Count the number of rows that meet the condition.
          *
-	     * @author Christopher T. Bishop
-	     * @since 0.1.0
-		 * @param string $condition
-		 * @return int
-		 */
-		public function count($condition = '') {
-			return $this->db->count($this->name, $condition);
-		}
+         * @author Christopher T. Bishop
+         * @since 0.1.0
+         * @param string $condition
+         * @return int
+         */
+        public function count($condition = '') {
+            return $this->db->count($this->name, $condition);
+        }
 
         /**
-		 * Inserts a row into the table.
+         * Inserts a row into the table.
          *
-	     * @author Christopher T. Bishop
-	     * @since 0.1.0
-		 * @param array $row
-		 * @return QueryResult The result with the row that was created as the first row.
-		 */
-		public function insert($row) {
-			$insertResult = $this->db->insert($this->name, $row);
-			$id = $insertResult->rows[0]['id'];
+         * @author Christopher T. Bishop
+         * @since 0.1.0
+         * @param array $row
+         * @return QueryResult The result with the row that was created as the first row.
+         */
+        public function insert($row) {
+            $insertResult = $this->db->insert($this->name, $row);
+            $id = $insertResult->rows[0]['id'];
 
-			$selectResult = $this->selectOne($id);
+            $selectResult = $this->selectOne($id);
 
-			$result = null;
-			if($selectResult->count() == 1) {
-				$result = new QueryResult($insertResult->resultSet, $selectResult->rows);
-			}
+            $result = null;
+            if($selectResult->count() == 1) {
+                $result = new QueryResult($insertResult->resultSet, $selectResult->rows);
+            }
 
-			return $result;
-		}
-
-        /**
-		 * Update all rows in the table that meet the condition.
-		 * If no condition is given then update all rows.
-         *
-	     * @author Christopher T. Bishop
-	     * @since 0.1.0
-		 * @param array $row
-		 * @param string $condition
-		 * @return QueryResult
-		 */
-		public function update($row, $condition) {
-			return $this->db->update($this->name, $row, $condition);
-		}
+            return $result;
+        }
 
         /**
-		 * Update a single row that meets the condition primaryKey=$value.
+         * Update all rows in the table that meet the condition.
          * If no condition is given then update all rows.
          *
-	     * @author Christopher T. Bishop
-	     * @since 0.1.0
-		 * @param array $row
-		 * @param mixed $value
-		 * @return QueryResult
-		 */
-		public function updateOne($row, $value) {
-			return $this->update($row, "$this->primaryKey='$value'");
-		}
+         * @author Christopher T. Bishop
+         * @since 0.1.0
+         * @param array $row
+         * @param string $condition
+         * @return QueryResult
+         */
+        public function update($row, $condition) {
+            return $this->db->update($this->name, $row, $condition);
+        }
 
         /**
-		 * Delete rows from the table that meet the condition.
+         * Update a single row that meets the condition primaryKey=$value.
+         * If no condition is given then update all rows.
          *
-	     * @author Christopher T. Bishop
-	     * @since 0.1.0
-		 * @param string $condition
-		 * @return QueryResult
-		 */
-		public function delete($condition) {
-			return $this->db->delete($this->name, $condition);
-		}
+         * @author Christopher T. Bishop
+         * @since 0.1.0
+         * @param array $row
+         * @param mixed $value
+         * @return QueryResult
+         */
+        public function updateOne($row, $value) {
+            return $this->update($row, "$this->primaryKey='$value'");
+        }
 
-		/**
-		 * Delete a single row that meets the condition primaryKey=$value.
+        /**
+         * Delete rows from the table that meet the condition.
          *
-	     * @author Christopher T. Bishop
-	     * @since 0.1.0
-		 * @param mixed $value
-		 * @return QueryResult
-		 */
-		public function deleteOne($value) {
-			return $this->delete("$this->primaryKey=$value");
-		}
+         * @author Christopher T. Bishop
+         * @since 0.1.0
+         * @param string $condition
+         * @return QueryResult
+         */
+        public function delete($condition) {
+            return $this->db->delete($this->name, $condition);
+        }
+
+        /**
+         * Delete a single row that meets the condition primaryKey=$value.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
+         * @param mixed $value
+         * @return QueryResult
+         */
+        public function deleteOne($value) {
+            return $this->delete("$this->primaryKey=$value");
+        }
 
     }
 
