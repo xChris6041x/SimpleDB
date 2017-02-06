9ffe2490728028c5fd2ef043f16dc392122fc60c
Christopher Bishop <christopherb6041@gmail.com>
2017-02-06 14:58:28 -0600

    Documented and reorganized.

--- src/Security.php
+++ src/Security.php
@@ -31,6 +31,9 @@
 
         /**
          * Log in the website using a name and password.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
          * @param string $name
          * @param string $password
          * @return bool Whether the user was able to log in with those credentials.
@@ -51,6 +54,9 @@
 
         /**
          * Check whether the credentials are valid in the database.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
          * @param string $name
          * @param string $password
          * @return int The ID of the account (or -1 for not valid).
@@ -67,6 +73,9 @@
 
         /**
          * Check whether the password for the logged in account is valid.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
          * @param Array $account
          * @param string $password
          */
@@ -76,6 +85,9 @@
 
         /**
          * Sign up to the website using a name, password and other information (for custom fields).
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
          * @param string $name
          * @param string $password
          * @param Array $info
@@ -96,6 +108,9 @@
 
         /**
          * Update the logged in user's password and other information.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
          * @param string $password
          * @param string $info
          * @return bool Whether the update was successful.
@@ -113,6 +128,9 @@
 
         /**
          * Update the a user's information.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
          * @param string $password
          * @param string $info
          * @return bool Whether the update was successful.
@@ -123,30 +141,10 @@
         }
 
         /**
-         * Log out of the website.
-         */
-        public static function logout(){
-            if(session_status() != PHP_SESSION_ACTIVE) return;
-            unset($_SESSION[SECURITY_LOGIN_NAME]);
-        }
-
-        /**
-         * Check whether a user is logged in.
-         * @return whether a user is logged in.
-         */
-        public static function isLoggedIn(){
-            if(session_status() != PHP_SESSION_ACTIVE) return false;
-
-            if(isset($_SESSION[SECURITY_LOGIN_NAME])){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-
-        /**
          * Get the logged in user row.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
          * @return NULL|array
          */
         public function getLoggedInAccount(){
@@ -160,6 +158,9 @@
 
         /**
          * Get an account by it's ID.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
          * @param int $id
          * @return NULL|array
          */
@@ -175,6 +176,9 @@
 
         /**
          * Get an account by it's name.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
          * @param string $name
          * @return Array|NULL The account as an array (or null if it can't be found).
          */
@@ -195,6 +199,9 @@
 
         /**
          * Check if an account has any role by it's ID.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
          * @param number $id
          * @param array $roles
          */
@@ -221,11 +228,44 @@
 
         /**
          * Check if a user has any role.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
          * @param array $roles
          */
         public function hasAnyRole($roles){
             if(session_status() != PHP_SESSION_ACTIVE || !isset($_SESSION[SECURITY_LOGIN_NAME])) return false;
             return $this->hasAnyRoleById($_SESSION[SECURITY_LOGIN_NAME], $roles);
+        }
+
+
+        /**
+         * Log out of the website.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
+         */
+        public static function logout(){
+            if(session_status() != PHP_SESSION_ACTIVE) return;
+            unset($_SESSION[SECURITY_LOGIN_NAME]);
+        }
+
+        /**
+         * Check whether a user is logged in.
+         *
+         * @author Christopher T. Bishop
+         * @since 0.1.0
+         * @return whether a user is logged in.
+         */
+        public static function isLoggedIn(){
+            if(session_status() != PHP_SESSION_ACTIVE) return false;
+
+            if(isset($_SESSION[SECURITY_LOGIN_NAME])){
+                return true;
+            }
+            else{
+                return false;
+            }
         }
 
      }
